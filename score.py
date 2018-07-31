5dcb7d81b2b272eca2c56a094173b49214b476f8
LI <1678530485@qq.com>
2018-07-31 12:39:44 +0800

    的

---
+++ score.py
@@ -0,0 +1,11 @@
+score=input()
+if 0<score<60:
+    print('等级是：','不及格')
+    elif 60<score<70:
+        print('等级是：','D')
+        elif 70<score<80:
+            print('等级是：','C')
+            elif 80<score<90:
+                print('等级是：','B')
+                else 90<score<100:
+                    print('等级是：','A')
